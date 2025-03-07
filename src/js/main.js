333d86e685e17670c07a869456cbb63bdddccd48
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2025-03-07 17:27:32 +0500

    slide update

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@
 var swiper = new Swiper(".cooperationSwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
-  loop: true,
   navigation: {
     nextEl: ".cooperation-next",
     prevEl: ".cooperation-prev",
