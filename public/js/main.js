6a21d49f81d3f722127a3dbabc6b147d815928b2
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2025-03-10 11:26:54 +0500

    calculator section the end

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
+document.querySelectorAll(".range-container").forEach(container => {
+  const slider = container.querySelector(".rangeSlider");
+  const priceValue = container.querySelector(".priceValue");
+  const progressBar = container.querySelector(".progressBar");
+  const thumb = container.querySelector(".thumb");
+
+  function updateSlider() {
+      const val = slider.value;
+      const min = slider.min;
+      const max = slider.max;
+      const percent = ((val - min) / (max - min)) * 100;
+
+      priceValue.textContent = (val * 1200).toLocaleString(); // Raqamni formatlash
+
+      progressBar.style.width = percent + "%";
+      thumb.style.left = `calc(${percent}% - 10px)`;
+  }
+
+  slider.addEventListener("input", updateSlider);
+  updateSlider(); // Boshlang'ich holatni o'rnatish
+});
+
+
 
 // cooperationSwiper
 var swiper = new Swiper(".cooperationSwiper", {
